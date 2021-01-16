24a0cfce9b29bbe67cb188653ac82b8d23161e3a
bogdan <f.alexandru13@yahoo.com>
2021-01-16 20:15:25 +0200

    -Configurari initiale -Creare view

---
+++ tema/src/utils/Config.java
@@ -0,0 +1,6 @@
+package utils;
+
+public class Config {
+    public static final String apiUrl = "http://api.openweathermap.org/data/2.5/weather?id=";
+    public static final String apiKey = "13adcf8af10ec9cdf3459a0c1d346758";
+}
